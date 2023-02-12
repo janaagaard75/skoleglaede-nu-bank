25befe32d86114247f920d83ab47d78e852abf99
Jan Aagaard <jan@aagaard.net>
2023-02-12 22:55:40 +0100

    Convert to functional component

--- src/TransferScreen.tsx
+++ src/TransferScreen.tsx
@@ -1,5 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Component } from "react";
+import { useState } from "react";
 import { Text, View } from "react-native";
 import { ifIphoneX } from "react-native-iphone-x-helper";
 import { HomeStackParamList } from "./App";
@@ -19,112 +19,83 @@ enum TransferAmount {
   TransferCustom = -1,
 }
 
-interface State {
-  selectedTransfer: TransferAmount;
-}
+export const TransferScreen = (props: Props) => {
+  const [selectedTransfer, setSelectedTransfer] = useState(TransferAmount.None);
 
-export class TransferScreen extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+  const transfer = () => {
+    Wallet.transferToSavings(selectedTransfer);
+    props.navigation.goBack();
+  };
 
-    this.state = {
-      selectedTransfer: TransferAmount.None,
-    };
-  }
-
-  private transfer() {
-    Wallet.transferToSavings(this.state.selectedTransfer);
-    this.props.navigation.goBack();
-  }
-
-  public render() {
-    return (
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
       <View
         style={{
-          backgroundColor: "#fff",
-          flex: 1,
+          marginHorizontal: 20,
+          marginTop: 30,
         }}
       >
-        <View
-          style={{
-            marginHorizontal: 20,
-            marginTop: 30,
-          }}
-        >
-          <Text>
-            Vælg hvor mange penge du vil overføre fra din konto til din
-            opsparing.
-          </Text>
-          <Text style={{ marginTop: 15 }}>
-            Konto: {Formatter.formatAsCurrency(Wallet.credit)}
-          </Text>
-          <Text style={{ marginTop: 5 }}>
-            Opsparing: {Formatter.formatAsCurrency(Wallet.savings)}
-          </Text>
-        </View>
-        <View
-          style={{
-            alignItems: "center",
-            alignSelf: "center",
-            flex: 1,
-            justifyContent: "center",
-            width: "50%",
-          }}
-        >
-          <TransferAmountButton
-            amount={TransferAmount.Transfer100}
-            onPress={() =>
-              this.setState({
-                selectedTransfer: TransferAmount.Transfer100,
-              })
-            }
-            selectedTransfer={this.state.selectedTransfer}
-          />
-          <TransferAmountButton
-            amount={TransferAmount.Transfer200}
-            onPress={() =>
-              this.setState({
-                selectedTransfer: TransferAmount.Transfer200,
-              })
-            }
-            selectedTransfer={this.state.selectedTransfer}
-          />
-          <TransferAmountButton
-            amount={TransferAmount.Transfer500}
-            onPress={() =>
-              this.setState({
-                selectedTransfer: TransferAmount.Transfer500,
-              })
-            }
-            selectedTransfer={this.state.selectedTransfer}
-          />
-          <TransferAmountButton
-            amount={TransferAmount.Transfer1000}
-            onPress={() =>
-              this.setState({
-                selectedTransfer: TransferAmount.Transfer1000,
-              })
-            }
-            selectedTransfer={this.state.selectedTransfer}
-          />
-        </View>
-        <View
-          style={{
-            marginBottom: ifIphoneX(50, 30),
-            paddingHorizontal: 20,
-            width: "100%",
-          }}
-        >
-          <SlideButton
-            disabled={this.state.selectedTransfer === TransferAmount.None}
-            onTrigger={() => this.transfer()}
-            title="Overfør"
-          />
-        </View>
+        <Text>
+          Vælg hvor mange penge du vil overføre fra din konto til din opsparing.
+        </Text>
+        <Text style={{ marginTop: 15 }}>
+          Konto: {Formatter.formatAsCurrency(Wallet.credit)}
+        </Text>
+        <Text style={{ marginTop: 5 }}>
+          Opsparing: {Formatter.formatAsCurrency(Wallet.savings)}
+        </Text>
       </View>
-    );
-  }
-}
+      <View
+        style={{
+          alignItems: "center",
+          alignSelf: "center",
+          flex: 1,
+          justifyContent: "center",
+          width: "50%",
+        }}
+      >
+        <TransferAmountButton
+          amount={TransferAmount.Transfer100}
+          onPress={() => setSelectedTransfer(TransferAmount.Transfer100)}
+          selectedTransfer={selectedTransfer}
+        />
+        <TransferAmountButton
+          amount={TransferAmount.Transfer200}
+          onPress={() => setSelectedTransfer(TransferAmount.Transfer200)}
+          selectedTransfer={selectedTransfer}
+        />
+        <TransferAmountButton
+          amount={TransferAmount.Transfer500}
+          onPress={() => setSelectedTransfer(TransferAmount.Transfer500)}
+          selectedTransfer={selectedTransfer}
+        />
+        <TransferAmountButton
+          amount={TransferAmount.Transfer1000}
+          onPress={() => setSelectedTransfer(TransferAmount.Transfer1000)}
+          selectedTransfer={selectedTransfer}
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: ifIphoneX(50, 30),
+          paddingHorizontal: 20,
+          width: "100%",
+        }}
+      >
+        <SlideButton
+          disabled={selectedTransfer === TransferAmount.None}
+          onTrigger={transfer}
+          title="Overfør"
+        />
+      </View>
+    </View>
+  );
+};
 
 const TransferAmountButton = (props: {
   amount: TransferAmount;

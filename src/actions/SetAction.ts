a3ab7e4d2c4ace1bb140df089db1683d1a79bd15
Jan Aagaard <jan@aagaard.net>
2023-02-12 21:33:00 +0100

    Remove TODO

--- src/actions/SetAction.ts
+++ src/actions/SetAction.ts
@@ -1,6 +1,5 @@
 import { Action } from "./Action";
 
-// TODO: Remove this action, since the app now has a reset function.
 export class SetAction extends Action {
   public get text(): string {
     return `Nulstil til ${this.formattedAmount}?`;

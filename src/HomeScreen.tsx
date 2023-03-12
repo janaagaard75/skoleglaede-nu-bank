2411966329120995fda8a6a2e01b3262444f001a
Jan Aagaard <jan@aagaard.net>
2023-03-12 22:32:31 +0100

    Convert to functional component

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,5 +1,4 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Component } from "react";
 import { Dimensions, View } from "react-native";
 import { ifIphoneX } from "react-native-iphone-x-helper";
 import { Account } from "./Account";
@@ -11,107 +10,95 @@ type Props = NativeStackScreenProps<HomeStackParamList, "HomeScreen"> & {
   savings: number;
 };
 
-interface State {
-  windowWidth: number;
-}
+export const HomeScreen = (props: Props) => {
+  const windowWidth = Dimensions.get("window").width;
 
-export class HomeScreen extends Component<Props, State> {
-  public constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      windowWidth: Dimensions.get("window").width,
-    };
-  }
-
-  public render() {
-    return (
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
       <View
         style={{
-          backgroundColor: "#fff",
-          flex: 1,
+          alignSelf: "flex-end",
+          paddingRight: 20,
+          paddingTop: 10,
         }}
       >
-        <View
-          style={{
-            alignSelf: "flex-end",
-            paddingRight: 20,
-            paddingTop: 10,
+        <Button
+          onPress={() => {
+            props.navigation.navigate("ResetScreen");
           }}
-        >
-          <Button
-            onPress={() => {
-              this.props.navigation.navigate("ResetScreen");
-            }}
-            fontSize={13}
-            title="Nulstil"
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-          }}
+          fontSize={13}
+          title="Nulstil"
         />
-        <Account
-          amount={this.props.credit}
-          icon="credit-card"
-          title="Konto"
-          windowWidth={this.state.windowWidth}
-        />
-        <Account
-          amount={this.props.savings}
-          icon="bank"
-          title="Opsparing"
-          windowWidth={this.state.windowWidth}
-        />
-        <View
-          style={{
-            flex: 1,
-          }}
-        />
-        <View
-          style={{
-            marginBottom: 10,
-            paddingHorizontal: 20,
-          }}
-        >
-          <Button
-            onPress={() => {
-              this.props.navigation.navigate("BrokeScreen");
-            }}
-            fontSize={16}
-            title="Fallit"
-          />
-        </View>
-        <View
-          style={{
-            marginBottom: 10,
-            paddingHorizontal: 20,
-          }}
-        >
-          <Button
-            onPress={() => {
-              this.props.navigation.navigate("TransferScreen");
-            }}
-            fontSize={16}
-            title="Overfør til opsparing"
-          />
-        </View>
-        <View
-          style={{
-            marginBottom: ifIphoneX(50, 30),
-            paddingHorizontal: 20,
-          }}
-        >
-          <Button
-            onPress={() => {
-              this.props.navigation.navigate("ScannerScreen");
-            }}
-            fontSize={16}
-            title="Scan QR-kode"
-          />
-        </View>
       </View>
-    );
-  }
-}
+      <View
+        style={{
+          flex: 1,
+        }}
+      />
+      <Account
+        amount={props.credit}
+        icon="credit-card"
+        title="Konto"
+        windowWidth={windowWidth}
+      />
+      <Account
+        amount={props.savings}
+        icon="bank"
+        title="Opsparing"
+        windowWidth={windowWidth}
+      />
+      <View
+        style={{
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          marginBottom: 10,
+          paddingHorizontal: 20,
+        }}
+      >
+        <Button
+          onPress={() => {
+            props.navigation.navigate("BrokeScreen");
+          }}
+          fontSize={16}
+          title="Fallit"
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: 10,
+          paddingHorizontal: 20,
+        }}
+      >
+        <Button
+          onPress={() => {
+            props.navigation.navigate("TransferScreen");
+          }}
+          fontSize={16}
+          title="Overfør til opsparing"
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: ifIphoneX(50, 30),
+          paddingHorizontal: 20,
+        }}
+      >
+        <Button
+          onPress={() => {
+            props.navigation.navigate("ScannerScreen");
+          }}
+          fontSize={16}
+          title="Scan QR-kode"
+        />
+      </View>
+    </View>
+  );
+};
